44bf40d314085c9c2e9443fbf41372e1f015ef80
mensenvau <otkir8311@gmail.com>
2023-08-20 07:35:07 +0500

    add

--- pages/done.js
+++ pages/done.js
@@ -3,10 +3,9 @@ import Image from 'next/image'
 
 export default function Home() {
 
-
     return (
         <div class="container-1">
-            <h1>Mofaqayatli ro'yxatdan o'tiz!</h1>
+            <h1>Arizangiz qabul qilindi.</h1>
         </div>
     )
 }
